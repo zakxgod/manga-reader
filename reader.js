fd36bc9b1665aa0538604d901f4c9ba4b4ced904
zakxgod <Romodin0707@mail.ru>
2026-09-19 17:59:44 +0400

    Add files via upload

--- reader.js
+++ reader.js
@@ -172,7 +172,7 @@
     // ==================== Настройки чтения ====================
 
     const READER_SETTINGS_KEY =
-        'manga_reader_settings_v1';
+        'manga_reader_settings_v2';
 
     const READER_FONT_MAP = {
         system: '',
